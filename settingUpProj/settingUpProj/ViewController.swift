887a24542882aa3d367cfc77406ee9d524dc6c8a
Anibal Ruiz <70773192+aruiz1174@users.noreply.github.com>
2023-08-17 00:06:47 -0400

    Add files via upload

---
+++ settingUpProj/settingUpProj/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  settingUpProj
+//
+//  Created by Anibal Ruiz on 8/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func changeBackgorundColor(_ sender: UIButton)
+    {
+        func changeColor() -> UIColor{
+
+                let red = CGFloat.random(in: 0...1)
+                let green = CGFloat.random(in: 0...1)
+                let blue = CGFloat.random(in: 0...1)
+
+                return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+            }
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+
+}
+
